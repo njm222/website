194de288907e65ce78ae667eb219e4ed40e04c8b
iinfin <48106228+iinfin@users.noreply.github.com>
2020-09-15 09:11:15 +0300

    move home components

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
 import Layout from '../components/layout';
-import Hero from '../components/hero';
-import Loading from '../components/loading';
-import Intro from '../components/intro';
-import Libs from '../components/libs';
-import Numbers from '../components/numbers';
+import Hero from '../components/home/hero';
+import Loading from '../components/home/loading';
+import Intro from '../components/home/intro';
+import Libs from '../components/home/libs';
+import Numbers from '../components/home/numbers';
 
 export default function Index() {
   return (
