a55586a8f43034b10cabfbc21cf47297a5bdc0d0
iinfin <48106228+iinfin@users.noreply.github.com>
2020-09-19 12:09:36 +0300

    edit intro boilerplate

--- components/intro/intro.js
+++ components/intro/intro.js
@@ -1,18 +1,7 @@
 function Intro() {
   return (
     <>
-      <div className="intro fixed left-0 top-0 right-0">
-        {/* animation */}
-        {/* animation */}
-      </div>
-      <style jsx>{`
-        .intro {
-          width: 100vw;
-          height: 100vh;
-          background-color: #f5f5f5;
-          z-index: 10;
-        }
-      `}</style>
+      <div className="fixed left-0 top-0 right-0 w-screen h-screen bg-c005 z-50">{/* animations */}</div>
     </>
   );
 }
