a61eb277c3eb7e6f74bcb4b5459cdaacd04f323c
iinfin <48106228+iinfin@users.noreply.github.com>
2020-09-19 12:11:06 +0300

    edit intro

--- components/intro/intro.js
+++ components/intro/intro.js
@@ -1,7 +1,11 @@
 function Intro() {
   return (
     <>
-      <div className="fixed left-0 top-0 right-0 w-screen h-screen bg-c005 z-50">{/* animations */}</div>
+      <div className="fixed left-0 top-0 right-0 w-screen h-screen bg-c005 z-50">
+        {/* Loading */}
+        {/* TitleFocus */}
+        {/* OtherScenes... */}
+      </div>
     </>
   );
 }
