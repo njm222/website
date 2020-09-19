f011bc8c029715ddfef5e635e7e8c0d61916ef6d
iinfin <48106228+iinfin@users.noreply.github.com>
2020-09-19 12:15:35 +0300

    edit title

--- components/home/home.js
+++ components/home/home.js
@@ -3,9 +3,7 @@ function Home() {
     <>
       <div className="fixed left-0 top-0 right-0">
         <div className="flex align-center justify-center w-screen h-screen">
-          <div className="m-auto">
-            <div className="text-lg font-semibold">Poimandres</div>
-          </div>
+          <div className="m-auto text-lg font-semibold">Poimandres</div>
         </div>
       </div>
     </>
